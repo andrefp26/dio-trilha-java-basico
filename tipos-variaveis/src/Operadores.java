46815530b33d95e3779e0791f8ccc6ab15b9a989
andrefp26 <andrefp26@gmail.com>
2023-03-01 19:18:23 -0400

    operadores teste

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int numero = 5;
+
+    //x repeticao
+    
+
+    System.out.println( numero -- );
+    System.out.println(numero);
+  }
+}
